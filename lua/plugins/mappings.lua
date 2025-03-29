7a055c9303f6c19016b1af2f0d9019a631751e69
Deepak Achariya <deepaka@enfabrica.net>
2025-03-29 12:36:36 -0700

    added mappings

---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,35 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["<F1>"] = { ":w!<cr>", desc = "Save" },
+          ["<F2>"] = { ":w!<cr>", desc = "Save" },
+          ["<F4>"] = { ":sp<cr>", desc = "Split" },
+          ["<F7>"] = { "<Leader>ff", desc = "Tab" },
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        i = {
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["<F1>"] = { "<esc>:w!<cr>", desc = "Save" },
+          ["<F2>"] = { "<esc>:w!<cr>", desc = "Save" },
+          ["<F4>"] = { "<esc>:sp<cr>", desc = "Split" },
+          ["<F7>"] = { "<esc><Leader>ff", desc = "Tab" },
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+        },
+      },
+    },
+  },
+}
